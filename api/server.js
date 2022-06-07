c3b9c009b89b5b1a7947248d83531afc830bb10c
cmj1980 <dagoclmt@gmail.com>
2022-06-06 18:19:29 -0700

    server running GET POST working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,55 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+const URL = "/api/users"
+
+server.use(express.json());
+
+server.get('/', (req, res)=>{
+    console.log('recevied a request')
+    res.json({message: 'recevied a request'});
+});
+
+server.get(URL, (req, res)=>{
+    Users.find().then(result =>{
+        if(result == null){
+            res.status(500).json({ message: "The users information could not be retrieved" });
+            return;
+        }
+        res.json(result)
+    });
+});
+
+server.get(`${URL}/:id`, (req, res)=>{
+    Users.findById(req.params.id).then(result => {
+        if(result == null){
+            res.status(404).json({ message: "The user with the specified ID does not exist" });
+            return;
+        }
+        res.json(result);
+    })
+    .catch(err =>{
+        res.status(500).json({ message: "The user information could not be retrieved" })
+    })
+});
+
+server.post(URL, (req, res)=>{
+    Users.insert(req.body).then(result =>{
+        if(!req.body.name || !req.body.bio){
+            res.status(400).json({ message: "Please provide name and bio for the user" });
+        }
+        res.status(201).json(result);
+    })
+    .catch(err =>{
+        res.status(500).json({ message: "There was an error while saving the user to the database" });
+    })
+});
+
+
+
+                                               
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
